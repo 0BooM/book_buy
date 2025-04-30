aaec7cc5ecc80cd5e53a0d216e517dd8cdff6eaf
0BooM <149960425+0BooM@users.noreply.github.com>
2025-04-30 16:30:24 +0200

    Create new route

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Book Buy</title>
+    <title>Book.buy | Home</title>
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
 </head>
 <body>

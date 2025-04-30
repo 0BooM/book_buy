12d58c027331c33971c304932fd1fa4cfa3cbd28
0BooM <149960425+0BooM@users.noreply.github.com>
2025-04-30 16:31:37 +0200

    New route page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book.buy | Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
